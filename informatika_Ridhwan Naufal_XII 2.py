4d778352e0ad770583286cd849b0c0db9489d14d
ridhwan15 <isyahman67@gmail.com>
2024-09-03 11:01:53 +0700

    Add files via upload

---
+++ informatika_Ridhwan Naufal_XII 2.py
@@ -0,0 +1,26 @@
+nama_variabel = input("assalamualaikum")
+nama = "Ridhwan Naufal Syahman"
+
+print ("hai", nama)
+
+
+
+
+
+penjualan = a + b           #15
+pengurangan = a - b         #5
+perkalian = a * b           #50
+pembagian = a / b           #2.0
+modulus = a % b             #0
+pangkat = a ** b            #100000
+pembagian_bulat = a // b    #2
+
+print("penjumlahan:", penjumlahan)
+print("pengurangan:", pengurangan)
+print("perkalian:", perkalian)
+print("pembagian:", pembagian)
+print("modulus:", modulus)
+print("panglat:", pangkat)
+print("pembagian_bulat:", pembagian_bulat)
+
+
